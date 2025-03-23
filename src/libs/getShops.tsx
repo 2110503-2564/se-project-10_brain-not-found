32b5bd530cbcafd73f0892ef7bf9d9fbe12ec83f
Rattapoom Phonyiam <lnwextra2@gamil.com>
2025-03-23 11:32:56 +0700

    adding new shop for admin

--- src/libs/getShops.tsx
+++ src/libs/getShops.tsx
@@ -1,7 +1,7 @@
 
 // src/libs/getShops.tsx
 export default async function getShops() {
-      const response = await fetch("https://project-sd-fronted-sub-backend.vercel.app/api/v1/shops");
+      const response = await fetch("https://project-sd-fronted-sub-backend.vercel.app/api/v1/shops" , {next : {tags:[`shops`]}});
       
       if (!response.ok) {
         throw new Error("Failed to fetch shop");
