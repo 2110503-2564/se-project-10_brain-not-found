5846920c1237b2766e9096675705fbb8b6eb4d5f
TeeRatTaPon27 <teerattapon080849@gmail.com>
2025-03-25 18:33:03 +0700

    Create addShopForAdmin

---
+++ src/app/addShopForAdmin/page.tsx
@@ -0,0 +1,17 @@
+// src/app/booking/manage/@dashboard/page.tsx
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
+import AddShopForm from '@/components/addShopForm';
+import UserProfile from '@/components/UserProfile';
+
+export default async function addShopForAdminPage() {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user.token) return null;
+
+  return (
+    <main className="bg-slate-500 m-5 p-5 rounded-lg">
+      <UserProfile />
+      {session.user.role === 'admin' && <AddShopForm />}
+    </main>
+  );
+}
--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -4,16 +4,22 @@ export default function TopMenuItem({ title, link }: { title: string; link: stri
   return (
     <>
       {
-      (title === "Massage")?(
-      <Link href={link} 
-        className="inline-block text-lg font-semibold text-orange-500 transition-all hover:text-orange-700 active:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300">
+        title === "Massage" ? (
+          <Link href={link} 
+            className="inline-block text-lg font-semibold text-orange-500 transition-all hover:text-orange-700 active:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300">
             {title}
-      </Link>)
-      : (
-        <Link href={link} 
-        className="inline-block text-lg font-semibold text-black transition-all hover:text-gray-700 active:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300">
+          </Link>
+        ) : title === "Admin Create Shop" ? (
+          <Link href={link} 
+            className="inline-block text-lg font-semibold text-red-500 transition-all hover:text-red-700 active:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300">
             {title}
-      </Link>)
+          </Link>
+        ) : (
+          <Link href={link} 
+            className="inline-block text-lg font-semibold text-black transition-all hover:text-gray-700 active:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300">
+            {title}
+          </Link>
+        )
       }
     </>
   );
