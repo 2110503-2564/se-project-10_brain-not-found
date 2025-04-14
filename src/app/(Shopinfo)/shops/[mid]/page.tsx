f846593376bbc6311d0877f8d9fbb6a678389695
SatanAlphabet <ori0pppppp@gmail.com>
2025-04-14 16:33:53 +0700

    re-arranged layout

--- src/app/(Shopinfo)/shops/[mid]/page.tsx
+++ src/app/(Shopinfo)/shops/[mid]/page.tsx
@@ -5,6 +5,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import { Paper, Typography, Grid, Box, Divider } from "@mui/material";
 import { orange, grey } from "@mui/material/colors";
+import { Suspense } from "react";
 
 export default async function ShopDetailPage({ params }: { params: { mid: string } }) {
     const shopDetail = await getVenue(params.mid);
@@ -57,70 +58,94 @@ export default async function ShopDetailPage({ params }: { params: { mid: string
 
                 <Grid container spacing={3} alignItems="center">
                     {/* Larger Image Column */}
-                    <Grid item xs={12} md={6}>
-                        <Box
-                            sx={{
-                                width: '100%',
-                                height: 'auto',
-                                borderRadius: 2,
-                                overflow: 'hidden',
-                                boxShadow: 2,
-                                // Add max height for image
-                                maxHeight: 500, // Adjust as needed
-                            }}
-                        >
-                            <Image
-                                src={(shopDetail.data.picture ?? '/img/logo.png')}
-                                alt={shopDetail.data.name}
-                                width={0}
-                                height={0}
-                                sizes="100vw"
-                                style={{ width: '100%', height: 'auto', objectFit: 'cover' }} // Add objectFit
-                            />
-                        </Box>
+                    <Grid item xs={12} md={4}>
+                        <ShopImage src={shopDetail.data.picture} alt={shopDetail.data.name}/>
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <ShopImage src={shopDetail.data.picture} alt={shopDetail.data.name}/>
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <ShopImage src={shopDetail.data.picture} alt={shopDetail.data.name}/>
                     </Grid>
                     {/* Smaller Detail Column */}
                     <Grid item xs={12} md={6}>
-                        <Box sx={{ textAlign: 'left', padding: 2 }}>
-                            <Typography variant="h5" component="div" sx={{ color: orange[700], fontWeight: 'bold' }}>
-                                {shopDetail.data.name}
-                            </Typography>
-                            <Typography variant="body1" sx={{ color: grey[800] }}>
-                                <b>Address:</b> {shopDetail.data.address}
-                            </Typography>
-                            <Typography variant="body1" sx={{ color: grey[800] }}>
-                                <b>District:</b> {shopDetail.data.district}
-                            </Typography>
-                            <Typography variant="body1" sx={{ color: grey[800] }}>
-                                <b>Province:</b> {shopDetail.data.province}
-                            </Typography>
-                            <Typography variant="body1" sx={{ color: grey[800] }}>
-                                <b>Region:</b> {shopDetail.data.region}
-                            </Typography>
-                            <Typography variant="body1" sx={{ color: grey[800] }}>
-                                <b>Postal Code:</b> {shopDetail.data.postalcode}
-                            </Typography>
-                            <Typography variant="body1" sx={{ color: grey[800] }}>
-                                <b>Tel:</b> {shopDetail.data.tel}
-                            </Typography>
-                            <Box sx={{ padding: 1 }}>
-                                <Typography variant="body1" sx={{ color: grey[800] }}>
-                                    <b>Open Time:</b> {shopDetail.data.openTime}
-                                </Typography>
-                                <Typography variant="body1" sx={{ color: grey[800] }}>
-                                    <b>Close Time:</b> {shopDetail.data.closeTime}
-                                </Typography>
-                            </Box>
+                        <ShopInfo info={shopDetail.data}/>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Box sx={{ padding: 3}}alignSelf={"flex-end"}>
+                            <BookingForm session={session} shop={shopDetail.data} />
                         </Box>
                     </Grid>
                 </Grid>
 
                 <Divider sx={{ my: 4, borderColor: orange[300] }} />
 
-                <Box sx={{ padding: 3 }}>
-                    <BookingForm session={session} shop={shopDetail.data} />
-                </Box>
             </Paper>
         </Box>
+    );
+}
+
+
+function ShopImage({src, alt} : {src: string, alt: string}) {
+    return (
+      <Box
+        sx={{
+          borderRadius: 2,
+          overflow: "hidden",
+          boxShadow: 2,
+          // Add max height for image
+          maxHeight: 350, // Adjust as needed
+          maxWidth: 350,
+        }}
+      >
+        <Image
+          src={src ?? "/img/logo.png"}
+          alt={alt}
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "auto", height: "auto", objectFit: "cover" }} // Add objectFit
+        />
+      </Box>
+    );
+}
+
+function ShopInfo({info} : {info: ShopItem}) {
+    return (
+      <Box sx={{ textAlign: "left", padding: 2 }}>
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{ color: orange[700], fontWeight: "bold" }}
+        >
+          {info.name}
+        </Typography>
+        <Typography variant="body1" sx={{ color: grey[800] }}>
+          <b>Address:</b> {info.address}
+        </Typography>
+        <Typography variant="body1" sx={{ color: grey[800] }}>
+          <b>District:</b> {info.district}
+        </Typography>
+        <Typography variant="body1" sx={{ color: grey[800] }}>
+          <b>Province:</b> {info.province}
+        </Typography>
+        <Typography variant="body1" sx={{ color: grey[800] }}>
+          <b>Region:</b> {info.region}
+        </Typography>
+        <Typography variant="body1" sx={{ color: grey[800] }}>
+          <b>Postal Code:</b> {info.postalcode}
+        </Typography>
+        <Typography variant="body1" sx={{ color: grey[800] }}>
+          <b>Tel:</b> {info.tel}
+        </Typography>
+        <Box sx={{ padding: 1 }}>
+          <Typography variant="body1" sx={{ color: grey[800] }}>
+            <b>Open Time:</b> {info.openTime}
+          </Typography>
+          <Typography variant="body1" sx={{ color: grey[800] }}>
+            <b>Close Time:</b> {info.closeTime}
+          </Typography>
+        </Box>
+      </Box>
     );
 }
